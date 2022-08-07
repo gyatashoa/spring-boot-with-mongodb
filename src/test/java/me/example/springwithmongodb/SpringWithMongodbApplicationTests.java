c49a89db23aa354982330eeef99ebb8609007f8b
Asamoah Yeboah Felix <felixasamoah539@gmail.com>
2022-08-07 20:04:02 +0000

    initial commit

---
+++ src/test/java/me/example/springwithmongodb/SpringWithMongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package me.example.springwithmongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWithMongodbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
